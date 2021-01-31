f566f2ab462d5ef261e95cc797c375a655e0ad6e
choudharymahipal <mschoudhary81@gmail.com>
2021-01-31 21:18:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angulareditor
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angulareditor)
---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, VERSION } from "@angular/core";
+
+import { Editor, Toolbar } from "ngx-editor";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  editor: Editor;
+  toolbar: Toolbar = [
+    ["bold", "italic"],
+    ["underline", "strike"],
+    ["code", "blockquote"],
+    ["ordered_list", "bullet_list"],
+    [{ heading: ["h1", "h2", "h3", "h4", "h5", "h6"] }],
+    ["link", "image"],
+    ["text_color", "background_color"],
+    ["align_left", "align_center", "align_right", "align_justify"]
+  ];
+  html: "";
+
+  ngOnInit(): void {
+    this.editor = new Editor();
+  }
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
+}
